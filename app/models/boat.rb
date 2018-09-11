f52369a11e66910e6f8abd2376d977835b6a81b8
Nicky Liu <nickyliu91@gmail.com>
2018-09-11 03:11:35 +0000

    automatically backed up by learn

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -2,4 +2,35 @@ class Boat < ActiveRecord::Base
   belongs_to  :captain
   has_many    :boat_classifications
   has_many    :classifications, through: :boat_classifications
+
+  def self.first_five
+    self.limit(5)
+  end
+
+  def self.dinghy
+    self.where('length < 20')
+  end
+
+  def self.ship
+    self.where('length > 20')
+  end
+
+  def self.last_three_alphabetically
+    self.order('name DESC').limit(3)
+  end
+
+  def self.without_a_captain
+    self.where(captain:nil)
+  end
+
+  def self.sailboats
+    boats = Arel::Table.new(:boats)
+    boat_classifications = Arel::Table.new(:boat_classifications)
+    classifications = Arel::Table.new(:classifications)
+    # binding.pry
+    id = Classification.select('id').where(name: 'Sailboat')
+    boat_classifications.join(:boats).on(boat_classifications[:boat_id].eq(boats[:id])).where(classifications[:boat_id].eq('#{id}'))
+    # y.where(classifications[:name].eq('Sailboat'))
+
+  end
 end
